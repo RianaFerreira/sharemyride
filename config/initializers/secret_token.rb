cdc42335e918a48472eb0a300ced9d6477cd9bdd
RianaFerreira <fuzzyri@Rianas-MacBook-Pro.local>
2013-11-17 11:53:23 +1100

    Devise gem setup and home page created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sharemyride::Application.config.secret_token = '516055becadf32f6acd5682d56311e84a7aeca83cf4f935c034acb003d849873309ada03af30f56b4e44ca6325cbe48b195d2255e51d56b394e97f0f180537e7'
