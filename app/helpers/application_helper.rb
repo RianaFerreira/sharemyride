7d9b5e0cbb332d154ea847a231621061449db63a
RianaFerreira <fuzzyri@Rianas-MacBook-Pro.local>
2013-11-21 10:18:12 +1100

    Added gravatars for users on trip index and show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 module ApplicationHelper
+  def avatar_url(user)
+    #create the gravitar
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
 
+    #link the gravitar to the user on the trip
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=mm"
+  end
 end
