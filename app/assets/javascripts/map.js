127d497a6b2f42a1d094483ed917f5667ca19956
RianaFerreira <rianaferreira@gmail.com>
2013-11-21 19:14:56 +1100

    Added maps and directions

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,48 +1,55 @@
-$(document).ready(function(){
- var map = new google.maps.Map(document.getElementById("map-canvas"), {
-  mapTypeId:google.maps.MapTypeId.ROADMAP});
-  var rendererOptions = { map: map };
-  var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+$(document).ready(function() {
+  if($('#myModal').length == 0){
+    //this javascript should only run when map modal is in the DOM
+    return;
+  }
+
+  var directionsDisplay = new google.maps.DirectionsRenderer();
   var directionsService = new google.maps.DirectionsService();
- $('#myModal').on('opened', function () {
-  google.maps.event.trigger(map, "resize");
-});
 
- $('.map-modal').on('click', function(event){
-    var self = $(this);
-    var url = self.attr('href');
+  var map = new google.maps.Map(document.getElementById("map-canvas"), {
+    mapTypeId:google.maps.MapTypeId.ROADMAP});
+    var url;
 
-    $.ajax({
-      type: "GET",
-      url: url,
-      dataType: "json",
-      success: function(response){
-        console.log(response);
-        var org = new google.maps.LatLng (response.locations[0].lat, response.locations[0].long);
-        var dest = new google.maps.LatLng (response.locations[1].lat, response.locations[1].long);
+    directionsDisplay.setMap(map);
+
+    $('.map-modal').on('click', function(event){
+      event.preventDefault();
+      var self = $(this);
+      url = self.attr('href');
+      $('#myModal').foundation('reveal', 'open');
+    });
+
+    $('#myModal').on('opened', function () {
+      google.maps.event.trigger(map, "resize");
+      $.ajax({
+        type: "GET",
+        url: url,
+        dataType: "json",
+        success: function(response){
+          var org = new google.maps.LatLng (response.locations[0].lat, response.locations[0].long);
+          var dest = new google.maps.LatLng (response.locations[1].lat, response.locations[1].long);
+
+          var request = {origin: org,
+            destination: dest,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+          };
 
 
-        var request = {origin: org,
-          destination: dest,
-          travelMode: google.maps.DirectionsTravelMode.DRIVING
-        };
+          directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+              directionsDisplay.setDirections(response);
+           } else {
+              alert ('failed to get directions');
+           }
+          });
 
+        }
 
-        directionsService.route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            map.fitBounds(response.routes[0].bounds);
-            map.panToBounds(response.routes[0].bounds);
-          }
-          else
-            alert ('failed to get directions');
-        });
-
-        $('#myModal').foundation('reveal', 'open');
-      }
+      });
 
     });
 
-    event.preventDefault();
-  });
 });
+
+
