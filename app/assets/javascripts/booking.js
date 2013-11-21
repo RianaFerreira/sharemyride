0acf23f85ff423e6751bd7e92c082845d1bb6e51
RianaFerreira <fuzzyri@Rianas-MacBook-Pro.local>
2013-11-21 13:37:59 +1100

    replaced new trip view with grid layout

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -2,14 +2,12 @@ $(document).ready(function(){
   $('.btn_book').click(function(event){
     var self = $(this);
     var url = self.attr('href');
-    console.log(url);
 
     $.ajax({
       type: "POST",
       url: url,
       dataType: "json",
       success: function(response){
-        console.log(response);
         //decrement available number of seats on the page
         $('#seats-'+response.id).html(response.seats_available);
         //self.parent().parent().children('.seats_available').html(response.seats_available);
